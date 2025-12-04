17af467abba1aa5e7e4d93cb341605852211a2a5
alexisarciniegas12-png <alexisarciniegas12@gmail.com>
2025-12-04 07:51:32 -0500

    Update edit_producto.php

--- edit_producto.php
+++ edit_producto.php
@@ -2,19 +2,26 @@
 include('connection.php');
 $con = connection();
 
+$id_producto = $_POST['id_producto'] ?? '';
+$nombre = $_POST['nombre'] ?? '';
+$precio = $_POST['precio'] ?? '';
+$stock = $_POST['stock'] ?? '';
+$id_tipo = $_POST['id_tipo'] ?? '';
+$id_usuario = $_POST['id_usuario'] ?? '';
 
-$id_producto = $_POST['id_producto'];
-$nombre = $_POST['nombre'];
-$precio = $_POST['precio'];
-$stock = $_POST['stock'];
-$id_tipo = $_POST['id_tipo'];
-$id_usuario = $_POST['id_usuario'];
+$sql = "UPDATE producto SET nombre=?, precio=?, stock=?, id_tipo=?, id_usuario=? WHERE id_producto=?";
+$stmt = mysqli_prepare($con, $sql);
 
-$sql = "UPDATE producto SET nombre='$nombre', precio='$precio', stock='$stock', id_tipo='$id_tipo', id_usuario='$id_usuario' WHERE id_producto='$id_producto'";
-$query = mysqli_query($con, $sql);
+$tipos = "ssiiii";
+mysqli_stmt_bind_param($stmt, $tipos, $nombre, $precio, $stock, $id_tipo, $id_usuario, $id_producto);
+
+$query = mysqli_stmt_execute($stmt);
+
+mysqli_stmt_close($stmt);
 
 if($query){
     Header("Location: index_producto.php");
 };
 
+mysqli_close($con);
 ?>
